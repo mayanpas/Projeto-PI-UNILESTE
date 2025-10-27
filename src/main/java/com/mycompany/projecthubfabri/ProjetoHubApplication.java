c01a54e85ecb5cbe93e69c2b0b02933d8358bbce
Thiago Dias <thiago.jardim@a.unileste.edu.br>
2025-10-27 19:25:43 -0300

    Initial Commit

---
+++ src/main/java/com/mycompany/projecthubfabri/ProjetoHubApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany.projecthubfabri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoHubApplication.class, args);
+        System.out.println("🚀 Aplicação iniciada!");
+        System.out.println("📍 Acesse: http://localhost:8080");
+        System.out.println("📍 Demo completo: http://localhost:8080/demo");
+    }
+}
